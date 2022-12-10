32b248af3efebeff4688046971eb3051d2c07eb1
Jeong_hyunwoo <rlck51@naver.com>
2022-12-10 19:16:28 +0900

    3강 버블 정렬

---
+++ src/main/java/algorithmTest/BubbleSort.java
@@ -0,0 +1,22 @@
+package algorithmTest;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int temp;
+        int[] array = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+
+        for(int i =0; i < 10; i++) {
+            for(int j = 0; j < 9-i; j++) {
+                if(array[j] > array[j+1]) {
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+
+        System.out.println("array = " + Arrays.toString(array));
+    }
+}
