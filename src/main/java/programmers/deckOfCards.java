9b2717790ad8604080aefa9e98d03f52be7f8a8e
Jeong_hyunwoo <rlck51@naver.com>
2023-02-20 10:55:54 +0900

    카드 뭉치

---
+++ src/main/java/programmers/deckOfCards.java
@@ -0,0 +1,25 @@
+package programmers;
+
+public class deckOfCards {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "Yes";
+        int i = 0;
+        int j = 0;
+
+        int k = 0;
+        while(k < goal.length) {
+            if(i < cards1.length && goal[k].equals(cards1[i])) {
+                i++;
+            } else if(j < cards2.length && goal[k].equals(cards2[j])) {
+                j++;
+            } else {
+                answer = "No";
+                break;
+            }
+
+            k++;
+        }
+
+        return answer;
+    }
+}
