8b3d297fedaf4e45684208ee97dee6a28be79260
Jeong_hyunwoo <rlck51@naver.com>
2023-02-21 09:41:15 +0900

    숫자 문자열과 영단어

---
+++ src/main/java/programmers/stringOfNumbersAndWords.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class stringOfNumbersAndWords {
+    public int solution(String s) {
+        String[] arr= {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for(int i =0; i <10;i++) {
+            s= s.replace(arr[i], Integer.toString(i));
+        }
+
+        int answer = Integer.parseInt(s);
+
+        return answer;
+    }
+}
