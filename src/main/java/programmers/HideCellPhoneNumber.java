766d8822efc694ac46ddab8e620bcfeb32c14cd1
Jeong_hyunwoo <rlck51@naver.com>
2023-03-24 06:45:46 +0900

    핸드폰 번호 가리기

---
+++ src/main/java/programmers/HideCellPhoneNumber.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class HideCellPhoneNumber {
+    public String solution(String phone_number) {
+        String answer = "";
+
+        for(int i=0;i<phone_number.length()-4;i++){
+            answer += "*";
+        }
+        answer += phone_number.substring(phone_number.length()-4,phone_number.length());
+
+        return answer;
+    }
+}
